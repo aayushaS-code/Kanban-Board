4ba08f4163fd3194f83b135297f20f4cccd2a8ad
Aayusha Shrestha <aayushashrestha069@gmail.com>
2026-01-21 21:38:55 +0545

    feat:clickable add-task-btn to add tasks

--- script.js
+++ script.js
@@ -1,44 +1,72 @@
-const todo=document.querySelector("#todo");
-const progress=document.querySelector("#progress");
-const done=document.querySelector("#done");
+const todo = document.querySelector("#todo");
+const progress = document.querySelector("#progress");
+const done = document.querySelector("#done");
 
-let dragElement=null;
+let tasksData = {};
+const columns = [todo, progress, done];
+let dragElement = null;
 
-const tasks=document.querySelectorAll(".task");
-tasks.forEach((task)=>{
-    task.addEventListener("drag",(e)=>{
-        // console.log("Dragging",e ,task)
-        dragElement=task;
-    })
-})
+if (localStorage.getItem("tasks")) {
+  const data = JSON.parse(localStorage.getItem("tasks"));
 
-function addDragElementsOnColumn(column){
-    column.addEventListener("dragenter",(e)=>{
-        e.preventDefault();
-        // console.log("hsjhdsd")
-        column.classList.add("hover-over")
-    })
+  for (const col in data) {
+    const column = document.querySelector(`#${col}`);
 
-    column.addEventListener("dragleave",(e)=>{
-        e.preventDefault();
-        column.classList.remove("hover-over");
-    })
+    data[col].forEach((task) => {
+      const div = document.createElement("div");
+      div.innerHTML = `  <h2>${task.title}</h2>
+                    <p>${task.desc}</p>
+                    <button>Delete</button>`;
 
-    column.addEventListener("dragover",(e)=>{
-        e.preventDefault();
-        // console.log("kjkdsd");
+      div.classList.add("task");
+      div.setAttribute("draggable", "true");
+      div.addEventListener("drag", (e) => {
+        dragElement = div;
+      });
 
-    })
+      column.appendChild(div);
 
-    column.addEventListener("drop",(e)=>{
-        e.preventDefault();
-        // console.log("kjksjd",e)
-        column.appendChild(dragElement);
-        column.classList.remove("hover-over");
+    });
+  }
+}
 
-    })
+const tasks = document.querySelectorAll(".task");
+tasks.forEach((task) => {
+  task.addEventListener("drag", (e) => {
+    // console.log("Dragging",e ,task)
+    dragElement = task;
+  });
+});
 
+function addDragElementsOnColumn(column) {
+  column.addEventListener("dragenter", (e) => {
+    e.preventDefault();
+    // console.log("hsjhdsd")
+    column.classList.add("hover-over");
+  });
 
+  column.addEventListener("dragleave", (e) => {
+    e.preventDefault();
+    column.classList.remove("hover-over");
+  });
+
+  column.addEventListener("dragover", (e) => {
+    e.preventDefault();
+    // console.log("kjkdsd");
+  });
+
+  column.addEventListener("drop", (e) => {
+    e.preventDefault();
+    // console.log("kjksjd",e)
+    column.appendChild(dragElement);
+    column.classList.remove("hover-over");
+    columns.forEach((col) => {
+      const tasks = col.querySelectorAll(".task");
+      const count = col.querySelector(".right");
+
+      count.innerText = tasks.length;
+    });
+  });
 }
 
 addDragElementsOnColumn(todo);
@@ -46,14 +74,52 @@ addDragElementsOnColumn(progress);
 addDragElementsOnColumn(done);
 
 // Modal-Logic
-const toggleModalButton=document.querySelector("#toggle-modal");
-const modalBg=document.querySelector(".modal .bg");
-const modal=document.querySelector(".modal")
+const toggleModalButton = document.querySelector("#toggle-modal");
+const modalBg = document.querySelector(".modal .bg");
+const modal = document.querySelector(".modal");
 
-toggleModalButton.addEventListener("click",(e)=>{
-    modal.classList.toggle("active");
-})
+toggleModalButton.addEventListener("click", (e) => {
+  modal.classList.toggle("active");
+});
 
-modalBg.addEventListener("click",(e)=>{
-    modal.classList.remove("active");
-})
+modalBg.addEventListener("click", (e) => {
+  modal.classList.remove("active");
+});
+
+const addTaskButton = document.querySelector(".modal .add-task-button");
+addTaskButton.addEventListener("click", (e) => {
+  const taskTitle = document.querySelector("#task-title-input").value;
+  const taskDesc = document.querySelector("#task-desc-input").value;
+
+  const div = document.createElement("div");
+  div.innerHTML = `  <h2>${taskTitle}</h2>
+                    <p>${taskDesc}</p>
+                    <button>Delete</button>`;
+
+  div.classList.add("task");
+  div.setAttribute("draggable", "true");
+  div.addEventListener("drag", (e) => {
+    dragElement = div;
+  });
+
+  todo.appendChild(div);
+  modal.classList.remove("active");
+
+  columns.forEach((col) => {
+    const tasks = col.querySelectorAll(".task");
+    const count = col.querySelector(".right");
+
+    tasksData[col.id] = Array.from(tasks).map((t) => {
+      return {
+        title: t.querySelector("h2").innerText,
+        desc: t.querySelector("p").innerText,
+      };
+    });
+
+    localStorage.setItem("tasks", JSON.stringify(tasksData));
+
+    // console.log(tasksData)
+
+    count.innerText = tasks.length;
+  });
+});
